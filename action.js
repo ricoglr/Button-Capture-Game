a0f5c4ad934f6887259e6ad3b475af4ab043b46c
bekirglr <bbekirglr@gmail.com>
2022-08-10 20:58:57 +0300

    I made a game

---
+++ action.js
@@ -0,0 +1,45 @@
+
+
+const captureBtn = document.getElementById("captureBtn");
+
+function ID(id){
+    return document.getElementById(id);
+}
+
+
+
+var point = 1000;
+ID("scoreScreen").innerText = point;
+
+
+
+captureBtn.addEventListener("mouseover",(x,y)=>{
+    x = (Math.random()*window.innerWidth-100)+150;
+    y = Math.random()*700;
+    captureBtn.style.top = y+"px";
+    captureBtn.style.left = x+"px";
+    point = point-100
+    ID("scoreScreen").innerText = point;
+
+    if(point<=0){
+        captureBtn.style.display ="none";
+        ID("lose").style.display = "flex";
+        ID("loseScore").innerText ="Your Point : "+ point;
+    }
+})
+
+
+captureBtn.addEventListener("click", ()=>{
+    captureBtn.style.display="none";
+    ID("lose").style.display = "flex";
+    ID("loseScore").innerText ="Congratulations : "+ point;
+})
+
+
+
+ ID("loseBtn").addEventListener("click", ()=>{
+     point = 1000;
+     ID("scoreScreen").innerText = point;
+     captureBtn.style.display="initial";
+     ID("lose").style.display="none";
+ });
